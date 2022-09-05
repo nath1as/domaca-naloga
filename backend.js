30199772ef1bda4449ac0c5e77a8c8b41437af32
nathias <n@th1.as>
2022-09-05 13:44:44 +0200

    Fix edge cases

--- backend.js
+++ backend.js
@@ -1,17 +1,27 @@
 const fileTransfer = (s, m, c) => {
+  if (s <= 0 || isNaN(s)) return -1;
+  if (c < 0 || isNaN(c)) return -1;
+  if (!m?.length) return -1;
+
   const transferred = m.reduce((a, b) => a + b, 0)
   const remaining = s - transferred
-  const arrayOfC = [...Array(c).keys()]
 
+  if (remaining == 0) return 0;
+
+  const arrayOfC = [...Array(c).keys()]
   const lastC = arrayOfC.map((_, i) => {
-    return m[m.length - (i + 1)]
+    return m[m.length - (i + 1)] ?? 0
   })
   const lastCsum = lastC.reduce((a, b) => {
     return a + b
   }, 0)
   const averageOfC = lastCsum / c
+  const timeRemaining = Math.ceil(remaining / averageOfC)
 
-  return Math.ceil(remaining / averageOfC)
+  if (timeRemaining === Infinity) return -1;
+  if (isNaN(timeRemaining)) return -1;
+
+  return timeRemaining
 }
 
 const s = 200
